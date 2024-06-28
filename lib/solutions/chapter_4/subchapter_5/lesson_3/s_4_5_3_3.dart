c7c100693bdf730c2455a2953337e2a1acea9bf7
antoniotaskov <antoniotaskov87@gmail.com>
2024-06-28 15:51:31 +0200

    new

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,17 +4,18 @@ class S4533 extends StatelessWidget {
   const S4533({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var container = Container(
-        height: 150,
-        width: 150,
-        color: Colors.blue,
-        child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("App"),
-              Text("Akademie"),
-            ]));
-    return container;
+    return Container(
+      height: 150,
+      width: 150,
+      color: Colors.blue,
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("App"),
+          Text("Akademie"),
+        ],
+      ),
+    );
   }
 }
